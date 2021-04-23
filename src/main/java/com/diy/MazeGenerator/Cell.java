d16dbec8bbc1d1dc3c4fcb53a39a6963168204f5
ndeely <nialldeely@gmail.com>
2021-04-23 16:32:10 +0100

    MazeGenerator additions
    added MazeGenerator methods:
    newMaze
    generateMaze
    isAvailable
    moveBall
    moveTo
    checkIfWin
    
    added methods to Cell:
    paintComponent
    hasAllWalls
    removeWall
    openTo
    setCurrent
    setEnd
    addPath

--- src/main/java/com/diy/MazeGenerator/Cell.java
+++ src/main/java/com/diy/MazeGenerator/Cell.java
@@ -16,6 +16,11 @@ public class Cell extends JPanel {
 
     private boolean[] wall = {true, true, true, true};
 
+    private boolean current = false;
+    private boolean end = false;
+
+    private boolean[] path = {false,false,false,false};
+
     public Cell(int row, int col) {
         this.row = row;
         this.col = col;
@@ -34,7 +39,92 @@ public class Cell extends JPanel {
     }
 
     public void paintComponent(Graphics g) {
-        g.fillRect(0, 0,SIZE,SIZE);
+        // draw the background
+        g.setColor(Color.WHITE);
+        g.fillRect(0,0, this.SIZE, this.SIZE);
+        g.setColor(Color.BLACK);
+
+        //draw the walls
+        if (this.wall[TOP]) {
+            g.drawLine(0, 0, this.SIZE - 1, 0);
+        }
+        if (this.wall[RIGHT]) {
+            g.drawLine(this.SIZE, 0, this.SIZE, this.SIZE);
+        }
+        if (this.wall[BOTTOM]) {
+            g.drawLine(0, this.SIZE, this.SIZE, this.SIZE);
+        }
+        if (this.wall[LEFT]) {
+            g.drawLine(0, 0, 0, this.SIZE);
+        }
+
+        // draw the path
+        g.setColor(Color.GREEN);
+        for (int i = 0; i < path.length; i++) {
+            if (path[i]) {
+                switch(i) {
+                    case TOP: g.drawLine(this.SIZE / 2, 0, this.SIZE / 2, this.SIZE / 2);
+                        break;
+                    case RIGHT: g.drawLine(this.SIZE, this.SIZE / 2, this.SIZE / 2, this.SIZE / 2);
+                        break;
+                    case BOTTOM: g.drawLine(this.SIZE / 2, this.SIZE, this.SIZE / 2, this.SIZE / 2);
+                        break;
+                    case LEFT: g.drawLine(0, this.SIZE / 2, this.SIZE / 2, this.SIZE / 2);
+                        break;
+                }
+            }
+        }
+
+        // draw the balls
+        if (this.current) {
+            g.setColor(Color.GREEN);
+            g.fillOval(3, 3, this.SIZE - 7, this.SIZE - 7);
+        } else if (this.end) {
+            g.setColor(Color.RED);
+            g.fillOval(3, 3, this.SIZE - 7, this.SIZE - 7);
+        }
+    }
+
+    public boolean hasAllWalls() {
+        return this.isWall(TOP) &&
+                this.isWall((RIGHT)) &&
+                this.isWall((BOTTOM)) &&
+                this.isWall((LEFT));
+    }
+
+    public void removeWall(int w) {
+        this.wall[w] = false;
+        this.repaint();
+    }
+
+    public void openTo(Cell neighbour) {
+        if (this.row < neighbour.getRow()) {
+            removeWall(BOTTOM);
+            neighbour.removeWall(TOP);
+        } else if (this.row > neighbour.getRow()) {
+            removeWall(TOP);
+            neighbour.removeWall(BOTTOM);
+        } else if (this.col < neighbour.getCol()) {
+            removeWall(RIGHT);
+            neighbour.removeWall(LEFT);
+        } else if (this.col > neighbour.getCol()) {
+            removeWall(LEFT);
+            neighbour.removeWall(RIGHT);
+        }
+    }
+
+    public void setCurrent(boolean current) {
+        this.current = current;
+        this.repaint();
+    }
+
+    public void setEnd(boolean end) {
+        this.end = end;
+        this.repaint();
+    }
+
+    public void addPath(int side) {
+        this.path[side] = true;
     }
 
     @Override
--- src/main/java/com/diy/MazeGenerator/MazeGenerator.java
+++ src/main/java/com/diy/MazeGenerator/MazeGenerator.java
@@ -4,9 +4,22 @@ import com.diy.mycomponents.TitleLabel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MazeGenerator extends JFrame {
     private static final long serialVersionUID = 1L;
+    private int rows = 30;
+    private int cols = 30;
+    private Cell[][] cells = new Cell[rows][cols];
+    private JPanel mazePanel = new JPanel();
+
+    private int row = 0;
+    private int col = 0;
+    private int endRow = this.rows - 1;
+    private int endCol = this.cols - 1;
 
     public static void main(String[] args) {
         try {
@@ -39,8 +52,145 @@ public class MazeGenerator extends JFrame {
         tl.setText("Maze");
         add(tl, BorderLayout.PAGE_START);
 
-        Cell cell = new Cell();
+        //center panel
+        JPanel centerPanel = new JPanel();
+        centerPanel.setBackground(Color.BLACK);
+        add(centerPanel, BorderLayout.CENTER);
 
-        add(cell, BorderLayout.CENTER);
+        //maze panel
+        newMaze();
+        centerPanel.add(mazePanel);
+
+        //button panel
+
+        //listeners
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                moveBall(e.getKeyCode());
+            }
+        });
+    }
+
+    private void newMaze() {
+        mazePanel.setLayout(new GridLayout(this.rows, this.cols));
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                cells[row][col] = new Cell(row, col);
+                mazePanel.add(cells[row][col]);
+            }
+        }
+        generateMaze();
+
+        this.row = 0;
+        this.col = 0;
+        this.endRow = this.rows - 1;
+        this.endCol = this.cols - 1;
+        this.cells[this.row][this.col].setCurrent(true);
+        this.cells[this.endRow][this.endCol].setEnd(true);
+    }
+
+    private void generateMaze() {
+        ArrayList<Cell> tryLater = new ArrayList<Cell>();
+        int totalCells = this.rows * this.cols;
+        int visitedCells = 1;
+        Random rand = new Random();
+        int r = rand.nextInt(this.rows);
+        int c = rand.nextInt(this.cols);
+
+        while(visitedCells < totalCells) {
+            // find neighbors with all walls intact
+            ArrayList<Cell> neighbors = new ArrayList<Cell>();
+            if (isAvailable(r - 1, c)) {
+                neighbors.add(cells[r - 1][c]);
+            }
+            if (isAvailable(r + 1, c)) {
+                neighbors.add(cells[r + 1][c]);
+            }
+            if (isAvailable(r, c - 1)) {
+                neighbors.add(cells[r][c - 1]);
+            }
+            if (isAvailable(r, c + 1)) {
+                neighbors.add(cells[r][c + 1]);
+            }
+
+            // if one or more was found
+            if (neighbors.size() > 0) {
+                // if more than one was found, add this cell to the list to try again
+                if (neighbors.size() > 1) {
+                    tryLater.add(cells[r][c]);
+                }
+                // pick a neighbor to remove the wall
+                Cell neighbor = neighbors.get(rand.nextInt(neighbors.size()));
+                cells[r][c].openTo(neighbor);
+
+                // go to the neighbor and increment visited
+                r = neighbor.getRow();
+                c = neighbor.getCol();
+                visitedCells++;
+            } else {
+                // go to a previously saved cell
+                Cell nextCell = tryLater.remove(0);
+                r = nextCell.getRow();
+                c = nextCell.getCol();
+            }
+        }
+    }
+
+    private boolean isAvailable(int r, int c) {
+        return r >= 0 &&
+                c >= 0 &&
+                r < this.rows &&
+                c < this.cols &&
+                cells[r][c].hasAllWalls();
+    }
+
+    private void moveBall(int direction) {
+        switch(direction) {
+            //up
+            case 38:
+            case 87:
+                if (!cells[this.row][this.col].isWall(Cell.TOP)) {
+                    this.moveTo(this.row - 1, this.col, Cell.TOP, Cell.BOTTOM);
+                }
+                break;
+            //right
+            case 39:
+            case 68:
+                if (!cells[this.row][this.col].isWall(Cell.RIGHT)) {
+                    this.moveTo(this.row, this.col + 1, Cell.RIGHT, Cell.LEFT);
+                }
+                break;
+            //down
+            case 40:
+            case 83:
+                if (!cells[this.row][this.col].isWall(Cell.BOTTOM)) {
+                    this.moveTo(this.row + 1, this.col, Cell.BOTTOM, Cell.TOP);
+                }
+                break;
+            //left
+            case 37:
+            case 65:
+                if (!cells[this.row][this.col].isWall(Cell.LEFT)) {
+                    this.moveTo(this.row, this.col - 1, Cell.LEFT, Cell.RIGHT);
+                }
+                break;
+        }
+        checkIfWin();
+    }
+
+    private void moveTo(int nextRow, int nextCol, int firstDirection, int secondDirection) {
+        cells[this.row][this.col].setCurrent(false);
+        cells[this.row][this.col].addPath(firstDirection);
+        cells[nextRow][nextCol].setCurrent(true);
+        cells[nextRow][nextCol].addPath(secondDirection);
+        this.row = nextRow;
+        this.col = nextCol;
+    }
+
+    private void checkIfWin() {
+        if (this.row == this.endRow && this.col == this.endCol) {
+            JOptionPane.showMessageDialog(this,"Congratulations, you win!");
+        }
     }
 }
